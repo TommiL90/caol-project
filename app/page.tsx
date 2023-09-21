68b4f0fddd74cc1f3ba68e780787fe2131d45776
Tomas Lillo Sanhueza <108248835+TommiL90@users.noreply.github.com>
2023-09-21 20:50:50 -0300

    feat: add Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,36 @@
-"use client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { FinancialProvider } from '@/contexts/financial-context'
+import ConsultantsComponent from '@/components/consultants-component'
+import { ClientsProvider } from '@/contexts/client-context'
+import ClientsComponent from '@/components/clients-component'
+import Image from 'next/image'
+import logo from '@/public/logo.gif'
 
-import { use, useEffect, useState } from "react"
-import { test } from "./prova"
-
-export default function Home() {
-  const [datax, setDatax] = useState<any[]>([])
-
-  useEffect(() => {
-   const getData = async () => {
-      const data = await test()
-      setDatax(data)
-    }
-    console.log('datax', datax)
-    getData()
-  }, [])
-
+export default async function Home() {
   return (
-    <main >
-      {JSON.stringify(datax, null, 2)}
+    <main className="flex min-h-screen flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">Financeiro</h2>
+          <Image src={logo} alt="Logo" width={100} height={100} />
+        </div>
+        <Tabs defaultValue="consultants">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="consultants">Consultores</TabsTrigger>
+            <TabsTrigger value="clients">Clientes</TabsTrigger>
+          </TabsList>
+          <TabsContent value="consultants">
+            <FinancialProvider>
+              <ConsultantsComponent />
+            </FinancialProvider>
+          </TabsContent>
+          <TabsContent value="clients">
+            <ClientsProvider>
+              <ClientsComponent />
+            </ClientsProvider>
+          </TabsContent>
+        </Tabs>
+      </div>
     </main>
   )
 }
