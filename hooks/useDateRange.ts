c1fb0cc290377c47a9251187ce396d9f1361dfe2
Tomas Lillo Sanhueza <108248835+TommiL90@users.noreply.github.com>
2023-09-24 22:38:56 -0300

    feat: format custom hook

--- hooks/useDateRange.ts
+++ hooks/useDateRange.ts
@@ -1,55 +1,56 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 export type DateRange = {
-  from: Date | undefined;
-  to?: Date | undefined;
-};
+  from: Date | undefined
+  to?: Date | undefined
+}
 
-export const useDateRange = (setDate: Dispatch<SetStateAction<DateRange | undefined>>) => {
-  const [startMonth, setStartMonth] = useState<string>('');
-  const [endMonth, setEndMonth] = useState<string>('');
+export const useDateRange = (
+  setDate: Dispatch<SetStateAction<DateRange | undefined>>,
+) => {
+  const [startMonth, setStartMonth] = useState<string>('')
+  const [endMonth, setEndMonth] = useState<string>('')
 
   const updateDateRange = () => {
-    const fromDateParts = startMonth.split('-');
-    const toDateParts = endMonth.split('-');
+    const fromDateParts = startMonth.split('-')
+    const toDateParts = endMonth.split('-')
 
     if (fromDateParts.length === 2 && toDateParts.length === 2) {
-      const fromYear = parseInt(fromDateParts[0]);
-      const fromMonth = parseInt(fromDateParts[1]) - 1;
-      const toYear = parseInt(toDateParts[0]);
-      const toMonth = parseInt(toDateParts[1]) - 1;
+      const fromYear = parseInt(fromDateParts[0])
+      const fromMonth = parseInt(fromDateParts[1]) - 1
+      const toYear = parseInt(toDateParts[0])
+      const toMonth = parseInt(toDateParts[1]) - 1
 
-      const fromDate = new Date(fromYear, fromMonth, 1);
-      const toDate = new Date(toYear, toMonth + 1, 0);
+      const fromDate = new Date(fromYear, fromMonth, 1)
+      const toDate = new Date(toYear, toMonth + 1, 0)
 
       if (fromDate > toDate) {
-        alert('A data de início não pode ser maior que a data de término');
+        alert('A data de início não pode ser maior que a data de término')
       }
 
-      setDate({ from: fromDate, to: toDate });
+      setDate({ from: fromDate, to: toDate })
     } else {
-      setDate({ from: undefined, to: undefined });
+      setDate({ from: undefined, to: undefined })
     }
-  };
+  }
 
   const handleStartMonthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setStartMonth(e.target.value);
-  };
+    setStartMonth(e.target.value)
+  }
 
   const handleEndMonthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEndMonth(e.target.value);
-  };
+    setEndMonth(e.target.value)
+  }
 
   useEffect(() => {
-    updateDateRange();
-  }, [startMonth, endMonth]);
+    updateDateRange()
+  }, [startMonth, endMonth])
 
-  // Exporta los valores de startMonth y endMonth
   return {
     startMonth,
     endMonth,
     handleStartMonthChange,
-    handleEndMonthChange
-  };
-};
+    handleEndMonthChange,
+  }
+}
